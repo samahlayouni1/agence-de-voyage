8255355a5049abf80ce7255e2e5fd487a441fad2
samah layouni <samahsamouha241@gmail.com>
2024-08-24 13:47:28 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';  // Importez le Provider de react-redux
 import './index.css';
 import App from '../src/components/App';
-import reportWebVitals from './reportWebVitals';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,4 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
